9c176827c110de698880687c68c404471e58a866
Nydroam <rayw.u17@gmail.com>
2016-02-22 21:25:00 -0500

    hw2 done

---
+++ hw02/anim.js
@@ -0,0 +1,33 @@
+var c = document.getElementById("slate");
+var ctx = c.getContext("2d");
+var button = document.getElementById("animate");
+var button1 = document.getElementById("stop");
+ctx.fillStyle = "#ff0000";
+var r = 0;
+var growing = true;
+var stop = false;
+
+var stopAnim = function(e){
+		stop = true;
+}
+var anim = function(e){
+	if(stop==false){
+		ctx.clearRect(0,0,500,500)
+		ctx.beginPath();
+		ctx.arc(250,250, r, 0, 2*Math.PI);
+		ctx.fill();
+		console.log(r);
+		if(growing)
+			r += 1;
+		else
+			r -= 1;
+		if(r>=250)
+			growing = false;
+		if(r<=0)
+			growing = true;
+		window.requestAnimationFrame(anim);
+	}
+	stop = false;
+}
+button.addEventListener("click", anim);
+button1.addEventListener("click", stopAnim);
